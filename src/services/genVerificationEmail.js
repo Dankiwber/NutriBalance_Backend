0f70a7dcceea7151dd6bd682d0a58e3f7c73df0d
Youming Zheng <sonnefzo@gmail.com>
2025-01-18 22:27:53 -0700

    bug fix

--- src/services/genVerificationEmail.js
+++ src/services/genVerificationEmail.js
@@ -50,7 +50,33 @@ const sendVerificationEmail = async (userId, email) => {
     }
 };
 
+const sendForgetPassEmail = async (userId, email) => {
+    try {
+        // 调用生成令牌的函数
+        const token = await VerifyToken(userId);
 
+        // 构造验证链接
+        const verificationLink = `http://${LOCAL_IP}:3000/api/verify-email?token=${token}`;
+
+        // 邮件内容
+        const mailOptions = {
+            from: process.env.EMAIL_USER, // 发送方
+            to: email,                   // 接收方
+            subject: 'Verify your email',       // 邮件主题
+            html: `
+                <p>Thank you for registering! Please click the link below to verify your email (valid for 5 minutes):</p>
+                <a href="${verificationLink}">${verificationLink}</a>
+            `
+        };
+
+        // 发送邮件
+        await transporter.sendMail(mailOptions);
+        console.log('Verification email sent to:', email);
+    } catch (err) {
+        console.error('Error sending verification email:', err.message);
+        throw new Error('Failed to send verification email.');
+    }
+};
 
 // 导出邮件发送器
-module.exports = {sendVerificationEmail };
+module.exports = {sendVerificationEmail, sendForgetPassEmail};
